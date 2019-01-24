f951ac5d88bec95d27e14d48ccf8b5f69095b8ae
nickelatomic28 <nickelatomic28@gmailcom>
2019-01-24 23:48:17 +0530

    my resume

---
+++ myresume/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+
+var app = express();
+
+app.use('/public',express.static('public'));
+
+app.get('/',function(req,res){
+    res.contentType("text/html");
+    res.sendfile(__dirname+'/index.html');
+})
+
+app.listen(3000,function(){
+    console.log("app listening on port 3000");
+});
+console.log("hello ganesha");
+
